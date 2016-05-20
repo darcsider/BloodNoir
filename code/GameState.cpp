a7f791c58e90eac7e6e5385fd914288617276980
DarcNess <jamie.cooper@gmail.com>
2016-05-20 08:36:58 -0500

    more updates and changes.

--- code/GameState.cpp
+++ code/GameState.cpp
@@ -5,3 +5,28 @@ $Creator: Jamie Cooper
 $Notice: (C) Copyright 2015 by Punch Drunk Squirrel Games LLC. All Rights Reserved.
 =====================================================================================*/
 #include "GameState.h"
+
+BannerParade::BannerParade()
+{
+
+}
+
+BannerParade::~BannerParade()
+{
+
+}
+
+void BannerParade::CallbackFunction()
+{
+	if (m_remainingBanners > 0)
+	{
+		m_nextBanner = true;
+		m_remainingBanners--;
+	}
+
+}
+
+void BannerParade::Update()
+{
+
+}
--- code/GameState.h
+++ code/GameState.h
@@ -8,6 +8,31 @@ $Notice: (C) Copyright 2015 by Punch Drunk Squirrel Games LLC. All Rights Reserv
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
 #include "Includes.h"
+#include "EventManager.h"
 
+class GameState
+{
+	protected:
 
+	public:
+		GameState() {}
+		virtual ~GameState() {}
+		virtual void Update() = 0;
+};
+
+class BannerParade : public GameState
+{
+	protected:
+		bool m_nextBanner;
+		int m_remainingBanners;
+		int m_numberOfBanners;
+		EventManager *m_eventManager;
+
+	public:
+		void CallbackFunction();
+		BannerParade();
+		~BannerParade();
+		void Update();
+
+};
 #endif
